e5c47fa04377c2697f1e5e667bb9b3b8963ae559
angelbejaranog <angelbejaranog@hotmail.com>
2019-06-23 00:36:45 -0400

    variable de entorno MONGO_URI

--- erver/config/config.js
+++ erver/config/config.js
@@ -22,8 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cfe";
 } else {
-  urlDB =
-    "mongodb+srv://yoyo:yoyo123@cluster0-6belp.mongodb.net/cafe?retryWrites=true&w=majority";
-}
+  urlDB = process.env.MONGO_URI;
+}    
 
 process.env.URLDB = urlDB;
